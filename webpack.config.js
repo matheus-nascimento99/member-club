42cc568e1f977733e724389f5935fb7aa68a271f
Matheus Nascimento Sergio <mnsergio59@gmail.com>
2024-07-30 09:27:57 -0300

    feat: app informations and webpack installation

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  target: "web",
+  mode: "development",
+  entry: path.resolve(__dirname, "src", "index.ts"),
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
